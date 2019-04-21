149086552f1bc0b645b7e53ba7ad8d514c6190fc
AlexPi <alex.pigida@outlook.com>
2019-04-21 12:55:41 -0400

    imgurl, subtask hier-hy pre-breakdown

--- material-hierarchy/src/app/model/base-task.ts
+++ material-hierarchy/src/app/model/base-task.ts
@@ -2,6 +2,7 @@ export class BaseTask {
   id: number;
   name: string;
   done: boolean;
+  imgurl: string;
   progress: number;
   subtasks: BaseTask[];
 }
--- material-hierarchy/src/app/service/mock-tasks.ts
+++ material-hierarchy/src/app/service/mock-tasks.ts
@@ -5,29 +5,53 @@ export const BaseTasksMock: BaseTask[] = [
     id: 1,
     name: 'Movers',
     done: false,
+    imgurl: 'https://weather.gc.ca/weathericons/small/01.png',
     progress: 33,
     subtasks: [
       {
         id: 11,
         name: 'Book',
         done: false,
+        imgurl: 'https://weather.gc.ca/weathericons/small/01.png',
         progress: 33,
         subtasks: [
-          { id: 111, name: 'Research', done: false, progress: 33, subtasks: null },
-          { id: 112, name: 'Interview', done: false, progress: 33, subtasks: null },
-          { id: 112, name: 'Negotiate', done: false, progress: 33, subtasks: null }
+          {
+            id: 111,
+            name: 'Research',
+            done: false,
+            imgurl: 'https://weather.gc.ca/weathericons/small/01.png',
+            progress: 33,
+            subtasks: null
+          },
+          {
+            id: 112,
+            name: 'Interview',
+            done: false,
+            imgurl: 'https://weather.gc.ca/weathericons/small/01.png',
+            progress: 33,
+            subtasks: null
+          },
+          {
+            id: 112,
+            name: 'Negotiate',
+            done: false,
+            imgurl: 'https://weather.gc.ca/weathericons/small/01.png',
+            progress: 33,
+            subtasks: null
+          }
         ]
       },
-      { id: 12, name: 'Exec', done: false, progress: 33, subtasks: null },
-      { id: 12, name: 'Part', done: false, progress: 33, subtasks: null }
+      { id: 12, name: 'Pack', done: false, imgurl: 'https://weather.gc.ca/weathericons/small/12.png', progress: 33, subtasks: null },
+      { id: 12, name: 'Move', done: false, imgurl: 'https://weather.gc.ca/weathericons/small/13.png', progress: 33, subtasks: null },
+      { id: 12, name: 'Part', done: false, imgurl: 'https://weather.gc.ca/weathericons/small/14.png', progress: 33, subtasks: null }
     ]
   },
-  { id: 2, name: 'Garbage', done: false, progress: 33, subtasks: null },
-  { id: 3, name: 'Address', done: false, progress: 33, subtasks: null },
-  { id: 4, name: 'Utilits', done: false, progress: 33, subtasks: null },
-  { id: 5, name: 'Internet', done: false, progress: 33, subtasks: null },
-  // { id: 6, name: 'Insurance', done: false, progress: 33, subtasks: null }
-  // { id: 7, name: 'Savings', done: false, progress: 33, subtasks: null },
-  // { id: 8, name: 'Tips', done: false, progress: 33, subtasks: null },
-  // { id: 9, name: 'Enjoy', done: false, progress: 33, subtasks: null }
+  { id: 2, name: 'Garbage ', done: false, imgurl: 'https://weather.gc.ca/weathericons/small/21.png', progress: 33, subtasks: null },
+  { id: 3, name: 'Address ', done: false, imgurl: 'https://weather.gc.ca/weathericons/small/22.png', progress: 33, subtasks: null },
+  { id: 4, name: 'Utilits ', done: false, imgurl: 'https://weather.gc.ca/weathericons/small/23.png', progress: 33, subtasks: null },
+  { id: 5, name: 'Internet', done: false, imgurl: 'https://weather.gc.ca/weathericons/small/24.png', progress: 33, subtasks: null }
+  // { id: 6, name: 'Insurance', done: false, imgurl: 'https://weather.gc.ca/weathericons/small/01.png', progress: 33, subtasks: null }
+  // { id: 7, name: 'Savings  ', done: false, imgurl: 'https://weather.gc.ca/weathericons/small/01.png', progress: 33, subtasks: null },
+  // { id: 8, name: 'Tips     ', done: false, imgurl: 'https://weather.gc.ca/weathericons/small/01.png', progress: 33, subtasks: null },
+  // { id: 9, name: 'Enjoy    ', done: false, imgurl: 'https://weather.gc.ca/weathericons/small/01.png', progress: 33, subtasks: null }
 ];
