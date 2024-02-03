9b54de62722dfd997226bd7fcddaf5e2b1d5780a
alexpisqueared <alex.pigida@outlook.com>
2024-02-03 09:20:48 -0500

    # 2024-02 Fully suspended in favor of permanent security.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # RBCx
 POCs, Demos, and such
+
+# 2024-02 Fully suspended in favor of permanent security.
--- material-hierarchy/src/app/model/base-task.ts
+++
@@ -1,8 +0,0 @@
-export class BaseTask {
-  id: number;
-  name: string;
-  done: boolean;
-  imgurl: string;
-  progress: number;
-  subtasks: BaseTask[];
-}
